28f235242e10cb8371643c65e9ed15acced1da58
NguyenMinhHieu2309 <nmh23092k6@gmail.com>
2025-10-28 16:20:15 +0700

    Add files via upload

---
+++ Bài 4/Bài 4 câu 1.py
@@ -0,0 +1,6 @@
+print("Sinh vien: Nguyen Minh Hieu")
+print("Mssv: 245752021610010")
+print("*************************************")
+S = input('Nhap chuoi:')
+for ch in S:
+       print(ch)
---
+++ Bài 4/Bài 4 câu 2.py
@@ -0,0 +1,7 @@
+print("Sinh vien: Nguyen Minh Hieu")
+print("Mssv: 245752021610010")
+print("*************************************")
+S = input('Nhap chuoi:')
+for ch in S:
+    if ch not in [' ', '\t']:
+        print(ch)
